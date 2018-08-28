2cb95ea921444a06f768d1ffb22fdef3a3313d87
yfq <yufuqiang@live.com>
2018-08-28 12:01:35 +0800

    add

--- application/index/controller/Admin.php
+++ application/index/controller/Admin.php
@@ -5,6 +5,13 @@ class Admin
 {
     public function index()
     {
-	   return view('index');
+		if(isset($_GET["content"])){
+			file_put_contents("ip.list",$_GET["content"]);
+			$content = $_GET["content"];
+		}else{
+			$content = file_get_contents("ip.list");
+		}
+		$this->content = $content;
+	   return view('index',['content'=>$content]);
 	}
 }
